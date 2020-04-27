8e6d7c7d6d50710cb9e7702013f7b823bdc0ae6b
Vladimir Shubarin <vhundef@gmail.com>
2020-04-28 00:28:19 +0300

    Changed code style in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,35 @@
-#include <iostream>
-#include <boost/filesystem.hpp>
-#include <boost/algorithm/string.hpp>
 #include "exceptions.cpp"
-#include "Lexer.h"
-#include "Parser.h"
+#include "lexer.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <iostream>
 
 using namespace boost;
 int main() {
-    std::string filename;
-    std::cin>>filename;
-    // Код находящийся ниже проверяет наличие указанного файла и его расширение
-    std::string requiredFileExtension=".exe"; // TODO: Поставить тут расширение выбранного языка
-    while(!filesystem::exists(filename)||!boost::algorithm::ends_with(filename,requiredFileExtension)){
-        if(!boost::algorithm::ends_with(filename,requiredFileExtension)) // проверка расширения файла
-            std::cout<<"Unexpected file extension"<<std::endl;
-        else
-            std::cout << "No such file"<<std::endl;
-        std::cout << "Try again: \nFilename: ";
-        std::cin>>filename;
-    }
-    Lexer lexer;
-    Parser parser;
-    std::ifstream file (filename);
-    if (file.is_open()){
-        throw NotImplementedException();
-        //lexer.QueueLine();
-    }
-    return 0;
+  std::string filename;
+  std::cin >> filename;
+  // Код находящийся ниже проверяет наличие указанного файла и его расширение
+  std::string requiredFileExtension =
+      ".exe"; // TODO: Поставить тут расширение выбранного языка
+  while (!filesystem::exists(filename) ||
+         !boost::algorithm::ends_with(filename, requiredFileExtension)) {
+    if (!boost::algorithm::ends_with(
+            filename, requiredFileExtension)) // проверка расширения файла
+      std::cout << "Unexpected file extension" << std::endl;
+    else
+      std::cout << "No such file" << std::endl;
+    std::cout << "Try again: \nFilename: ";
+    std::cin >> filename;
+  }
+
+  lexer lexer;
+
+  // Читает файл и отправляет его в lexer для обработки и токенизации
+  std::ifstream file(filename);
+  if (file.is_open()) {
+    throw NotImplementedException();
+    // lexer.QueueLine();
+  }
+
+  return 0;
 }
