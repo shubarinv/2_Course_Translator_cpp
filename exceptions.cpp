a0064b2544dea8fbae281910c2d117e69e1808e7
Vladimir Shubarin <vhundef@gmail.com>
2020-04-27 23:27:48 +0300

    Added NotImplementedException

---
+++ exceptions.cpp
@@ -0,0 +1,28 @@
+//
+// Created by vhund on 27.04.2020.
+//
+#include <stdexcept>
+
+class NotImplementedException : public std::logic_error
+{
+private:
+
+    std::string _text;
+
+    NotImplementedException(const char* message, const char* function): std::logic_error("Not Implemented")
+    {
+        _text = message;
+        _text += " : ";
+        _text += function;
+    };
+
+public:
+    NotImplementedException(): NotImplementedException("Not Implemented", __FUNCTION__){
+    }
+    explicit NotImplementedException(const char* message): NotImplementedException(message, __FUNCTION__){}
+
+    virtual const char *what() const throw()
+    {
+        return _text.c_str();
+    }
+};
