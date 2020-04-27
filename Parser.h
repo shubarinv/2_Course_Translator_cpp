fccc225e7313d1288e82225e983560f9a134862b
Vladimir Shubarin <vhundef@gmail.com>
2020-04-27 23:52:40 +0300

    Added Lexer class

---
+++ Parser.h
@@ -0,0 +1,16 @@
+//
+// Created by vhundef on 27.04.2020.
+//
+#ifndef SPO_COMPILER_PARSER_H
+#define SPO_COMPILER_PARSER_H
+
+
+class Parser {
+    public:
+        Parser(){
+            throw NotImplementedException();
+        }
+};
+
+
+#endif //SPO_COMPILER_PARSER_H
