5b8deaafa941f436219e7b72923a1ba55ae8e063
Vladimir Shubarin <vhundef@gmail.com>
2020-04-28 00:30:40 +0300

    Updated main.cpp code style again

--- main.cpp
+++ main.cpp
@@ -5,31 +5,30 @@
 #include <iostream>
 
 using namespace boost;
+
 int main() {
-  std::string filename;
-  std::cin >> filename;
-  // Код находящийся ниже проверяет наличие указанного файла и его расширение
-  std::string requiredFileExtension =
-      ".exe"; // TODO: Поставить тут расширение выбранного языка
-  while (!filesystem::exists(filename) ||
-         !boost::algorithm::ends_with(filename, requiredFileExtension)) {
-    if (!boost::algorithm::ends_with(
-            filename, requiredFileExtension)) // проверка расширения файла
-      std::cout << "Unexpected file extension" << std::endl;
-    else
-      std::cout << "No such file" << std::endl;
-    std::cout << "Try again: \nFilename: ";
+    std::string filename;
     std::cin >> filename;
-  }
+    // Код находящийся ниже проверяет наличие указанного файла и его расширение
+    std::string requiredFileExtension =
+            ".exe"; // TODO: Поставить тут расширение выбранного языка
+    while (!filesystem::exists(filename) || !boost::algorithm::ends_with(filename, requiredFileExtension)) {
+        if (!boost::algorithm::ends_with(filename, requiredFileExtension)) // проверка расширения файла
+            std::cout << "Unexpected file extension" << std::endl;
+        else
+            std::cout << "No such file" << std::endl;
+        std::cout << "Try again: \nFilename: ";
+        std::cin >> filename;
+    }
 
-  lexer lexer;
+    lexer lexer;
 
-  // Читает файл и отправляет его в lexer для обработки и токенизации
-  std::ifstream file(filename);
-  if (file.is_open()) {
-    throw NotImplementedException();
-    // lexer.QueueLine();
-  }
+    // Читает файл и отправляет его в lexer для обработки и токенизации
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        throw NotImplementedException();
+        // lexer.QueueLine();
+    }
 
-  return 0;
+    return 0;
 }
