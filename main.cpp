0a637111c5445b9dc7f742664d6d39e6df592c8a
Vladimir Shubarin <vhundef@gmail.com>
2020-04-27 23:27:56 +0300

    Added comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string.hpp>
+#include "exceptions.cpp"
 using namespace boost;
 int main() {
     std::string filename;
     std::cin>>filename;
-
-    std::string requiredFileExtension=".exe";
+    // Код находящийся ниже проверяет наличие указанного файла и его расширение
+    std::string requiredFileExtension=".exe"; // TODO: Поставить тут расширение выбранного языка
     while(!filesystem::exists(filename)||!boost::algorithm::ends_with(filename,requiredFileExtension)){
-        if(!boost::algorithm::ends_with(filename,requiredFileExtension))
+        if(!boost::algorithm::ends_with(filename,requiredFileExtension)) // проверка расширения файла
             std::cout<<"Unexpected file extension"<<std::endl;
         else
             std::cout << "No such file"<<std::endl;
         std::cout << "Try again: \nFilename: ";
         std::cin>>filename;
+    }
+    std::ifstream file (filename);
+    if (file.is_open()){
+        throw NotImplementedException();
+        file.close();
+        //lexer.QueueLine();
     }
     return 0;
 }
