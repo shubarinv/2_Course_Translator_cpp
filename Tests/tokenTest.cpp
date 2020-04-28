866fe8a123db4eaccbf1f97b4458c20e637f0a94
Vladimir Shubarin <vhundef@gmail.com>
2020-04-28 16:27:06 +0300

    Updated token tests

--- Tests/tokenTest.cpp
+++ Tests/tokenTest.cpp
@@ -4,18 +4,41 @@
 #include <Token.hpp>
 #include "gtest/gtest.h"
 
-TEST(token,idToken) {
-    EXPECT_EQ(Token::determineTokenType("i"), Token::tokenType::Id);
+TEST(token, unregisteredTokenTypeDetection) {
+    Token token("=");
+    EXPECT_EQ(token.getType(), Token::tokenType::Undefined);
 }
-TEST(token, assignmentToken) {
-    EXPECT_EQ(Token::determineTokenType(":="), Token::tokenType::Assignment);
+TEST(token, assignmentTokenTypeDetection) {
+    Token token(":=");
+    EXPECT_EQ(token.getType(), Token::tokenType::Assignment);
 }
-TEST(token, equalityToken) {
-    EXPECT_EQ(Token::determineTokenType("=="), Token::tokenType::Comparison);
+TEST(token, equalityTokenTypeDetection) {
+    Token token("==");
+    EXPECT_EQ(token.getType(), Token::tokenType::Comparison);
 }
-TEST(token, keywordToken) {
-    EXPECT_EQ(Token::determineTokenType("if"), Token::tokenType::Keyword);
+TEST(token, keywordTokenTypeDetection) {
+    Token token("if");
+    EXPECT_EQ(token.getType(), Token::tokenType::Keyword);
 }
-TEST(token, mathOpToken) {
-    EXPECT_EQ(Token::determineTokenType("+"), Token::tokenType::MathOp);
+TEST(token, mathOpTokenTypeDetection) {
+    Token token("+");
+    EXPECT_EQ(token.getType(), Token::tokenType::MathOp);
+}
+
+
+TEST(token, equalityTokenTypeTranslation) {
+    Token token("==");
+    EXPECT_EQ(Token::typeToString(token.getType()), "Comparison");
+}
+TEST(token, keywordTokenTypeTranslation) {
+    Token token("if");
+    EXPECT_EQ(Token::typeToString(token.getType()), "Keyword");
+}
+TEST(token, mathOpTokenTypeTranslation) {
+    Token token("+");
+    EXPECT_EQ(Token::typeToString(token.getType()), "MathOp");
+}
+TEST(token, idOpTokenTypeTranslation) {
+    Token token("i");
+    EXPECT_EQ(Token::typeToString(token.getType()), "Id");
 }
